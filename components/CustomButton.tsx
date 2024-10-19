7fa710e8d8dfb9800d1ca80c813d6bfe352228ff
sona678 <sonamaharjan5@gmail.com>
2024-10-19 17:05:16 +0545

    footer added

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React from 'react'
 import Image from 'next/image'
-import { CustomButtonProps } from '@/types'
+import { CustomButtonProps } from '@/types/indexes'
 
 const CustomButton = ({title, containerStyles, handleClick, btnType}:CustomButtonProps) => {
   return (
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -13,10 +13,10 @@ const Hero = () => {
       <div className='flex-1 pt-36 padding-x'>
         <h1 className='hero__title'>Find, book or rent a car- quickly and easily!!</h1>
         <p className='hero__subtitle'>Streamline your car rental experience with our effortless booking process..</p>
-        <CustomButton title="Explore Cars" containerStyles="bg-primary-blue text-white rounded-full mt-10" handleClick={handleScroll}/>
+        <CustomButton title="Explore Cars" containerStyles="flex bg-primary-blue text-white rounded-full mt-10" handleClick={handleScroll}/>
         <div className='hero__image-container'>
           <div className='hero__image'>
-            <Image src="/hero.png" alt="Car" fill className='object-contain'/>
+            <Image src="/hero.png" alt="Car" fill className='flex object-contain'/>
           </div>
         </div>
         <div className='hero__image-overlay'/>
