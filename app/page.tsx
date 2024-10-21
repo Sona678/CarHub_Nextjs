2253e729e5d252b28a12eea327a5f4471dbfa0a0
sona678 <sonamaharjan5@gmail.com>
2024-10-21 14:08:00 +0545

    Car filter

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,27 @@
 
 import Image from "next/image";
 import Hero from './../components/Hero';
+import SearchBar from "@/components/SearchBar";
+import CustomFilter from "@/components/CustomFilter";
 
 export default function Home() {
   return (
     <div>
       <main className="overflow-hidden">
       <Hero/>
+      <div className="mt-12 padding-x padding-y max-width" id="discover">
+        <div className="home__text-container">
+          <h1 className="text-3xl font-extrabold">Car Catalogue</h1>
+          <p>Explore the cars you might like</p>
+        </div>
+        <div className="home__filters">
+          <SearchBar/>
+          <div className="home__filter-container">
+            <CustomFilter title="fuel"/>
+            <CustomFilter title="year"/>
+          </div>
+        </div>
+      </div>
       </main>
     </div>
   )
